87ba57d7a8e49cc17e04b9f4b82288c4aa76d47e
Manisha K <manishakishtapuram24@gmail.com>
2025-06-11 13:48:38 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';  // Keep this import
 import { errorHandler } from '../utils/error.js';
-export const signup = async(req,res,next)=>{
-const {username, email, password} = req.body;
+import jwt from 'jsonwebtoken';
 
-if (
+export const signup = async(req,res,next)=>{
+  const {username, email, password} = req.body;
+
+  if (
     !username ||
     !email ||
     !password ||
@@ -14,17 +16,52 @@ if (
   ) {
     next(errorHandler(400,'All fields are required'));
   }
-  const hashedPassword = bcrypt.hashSync(password,10)
+  
+  const hashedPassword = bcrypt.hashSync(password,10);
   const newUser = new User({
     username,
     email,
     password: hashedPassword,
   });
+  
   try {
     await newUser.save();
-  res.json('Signup successful');
+    res.json('Signup successful');
   } catch (error) {
     next(error);
   }
 }
-export default signup;
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password); // Changed from bcryptjs to bcrypt
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
