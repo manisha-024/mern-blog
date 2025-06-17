989625fef057a83b7c729284a4fc9de3cd3e248c
Manisha K <manishakishtapuram24@gmail.com>
2025-06-17 12:42:13 +0530

    complete profile page ui

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,34 +1,49 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import {
   Sidebar,
   SidebarItem,
   SidebarItemGroup
 } from 'flowbite-react';
-import {HiArrowSmRight, HiUser} from 'react-icons/hi';
-import { Link, useLocation } from 'react-router-dom';
+import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 export default function DashSidebar() {
-  const location = useLocation
-    const [tab,setTab] = useState('');
-    useEffect(()=>{
-        const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
-        if (tabFromUrl){
-          setTab(tabFromUrl);
-        }
-    }, [location.search]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <Sidebar className='w-full md:w-56'>
-            <SidebarItemGroup>
-              <Link to='/dashboard?tab=profile'>
-                <SidebarItem active={tab=== 'profile'} icon = {HiUser} label={'User'} labelColor='dark'>
-                Profile
-                </SidebarItem>
-                </Link>
-                <SidebarItem icon = {HiArrowSmRight} className='cursor-pointer'>
-                Sign Out
-                </SidebarItem>
-
-            </SidebarItemGroup>
+      <SidebarItemGroup>
+        <SidebarItem
+          active={tab === 'profile'}
+          icon={HiUser}
+          label={'User'}
+          labelColor='dark'
+          onClick={() => navigate('/dashboard?tab=profile')}
+          className='cursor-pointer'
+        >
+          Profile
+        </SidebarItem>
+        <SidebarItem
+          icon={HiArrowSmRight}
+          className='cursor-pointer'
+          onClick={() => {
+            // Add sign out logic here
+            console.log('Sign out clicked');
+          }}
+        >
+          Sign Out
+        </SidebarItem>
+      </SidebarItemGroup>
     </Sidebar>
-  )
+  );
 }
