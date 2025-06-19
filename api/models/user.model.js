b2b7549869b828fbf8953e83e05534de23489ade
Manisha K <manishakishtapuram24@gmail.com>
2025-06-20 01:07:28 +0530

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         default: 
         'https://imgs.search.brave.com/l6KlMRmsGgJoxTyOOsg7GV55wujc5NhvTR_9Z021GjM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTg3/ODA1MTU2L3ZlY3Rv/ci9wcm9maWxlLXBp/Y3R1cmUtdmVjdG9y/LWlsbHVzdHJhdGlv/bi5qcGc_Yj0xJnM9/NjEyeDYxMiZ3PTAm/az0yMCZjPVNnbE1m/TGhWdEhySmVpMng5/YXozRndxaW42aWNw/RkRfMmxnRDBlcVpW/WkE9',
     }, 
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    }
     },{timestamps: true}
 );
 
