50e23b9cb62be48d840de0fafd1b9311790d7639
Manisha K <manishakishtapuram24@gmail.com>
2025-06-27 08:52:55 +0530

    add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -2,7 +2,7 @@ import { Alert, Button, Textarea } from 'flowbite-react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment';
 
 export default function CommentSection({ postId }) {
@@ -10,6 +10,7 @@ export default function CommentSection({ postId }) {
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
     const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -35,6 +36,34 @@ export default function CommentSection({ postId }) {
       }
     } catch (error) {
       setCommentError(error.message);
+    }
+  };
+
+   const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/signin');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   };
 
@@ -115,6 +144,7 @@ export default function CommentSection({ postId }) {
             <Comment
               key={comment._id}
               comment={comment}
+              onLike={handleLike}
             />
           ))}
         </>
