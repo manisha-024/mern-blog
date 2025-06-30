abbec5a3705580b80f8ff8fe16c1850e877565d2
Manisha K <manishakishtapuram24@gmail.com>
2025-06-30 21:04:11 +0530

    added for other ports

--- api/index.js
+++ api/index.js
@@ -25,8 +25,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+const port =process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 3000!');
 });
 
