927a900cf1d1c4645b877e9e22084308d559a6f4
Manisha K <manishakishtapuram24@gmail.com>
2025-06-30 10:14:20 +0530

    updated vite url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import ThemeProvider from './components/ThemeProvider.jsx';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
